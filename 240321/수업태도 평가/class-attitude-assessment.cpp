2fd49c0043325e40c5afb16a70e18886efb894ca
HanDaYeon/한다연 <75533232+HanDaYeon-coder@users.noreply.github.com>
2024-03-21 10:11:19 +0900

    Title: 수업태도 평가, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240321/수업태도 평가/class-attitude-assessment.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N = 0;
+string name;
+int grade;
+string names[7] = {"Bessie", "Elsie", "Daisy", "Gertie", "Annabelle", "Maggie", "Henrietta"};
+map<string, int> students;
+vector<pair<int, string>> grades;
+ 
+int main() {
+    cin >> N;
+    
+    // 1. 각 학생의 총합 점수 구하기
+    // 2. 
+
+    for(int i=0; i<N; i++) {
+        cin>>name>>grade;
+        students[name]+=grade;
+    }
+
+    for(int i=0; i<7; i++) {
+        grades.push_back({students[names[i]],names[i]});
+    }
+
+    sort(grades.begin(), grades.end());
+
+    int minGrade = grades[0].first;
+    // cout<<"min : "<<minGrade<<"\n";
+
+    int secondMinGrade = 0;
+    string answer;
+
+    for(int i=1; i<7; i++) {
+        if(minGrade != grades[i].first and secondMinGrade == 0) {
+            secondMinGrade = grades[i].first;
+            answer = grades[i].second;
+        }
+        else if(secondMinGrade != 0) {
+            if(secondMinGrade == grades[i].first) {
+                cout<<"Tie"<<"\n";
+            } else {
+                cout<<answer<<"\n";
+            }
+            break;
+        }
+    }
+
+    if(secondMinGrade == 0) { //다 동점인 경우
+        cout<<"Tie"<<"\n";
+    }
+
+    return 0;
+}
