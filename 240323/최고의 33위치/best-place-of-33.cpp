8f847e551d72d3e164b95c3b46f63ecce4e32a55
HanDaYeon/한다연 <75533232+HanDaYeon-coder@users.noreply.github.com>
2024-03-23 13:01:00 +0900

    Title: 최고의 33위치, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240323/최고의 33위치/best-place-of-33.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int n;
+int arr[24][24];
+
+int main() {
+    cin>>n;
+
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            cin>>arr[i][j];
+        }
+    }
+
+    int ans = 0;
+    for(int i=0; i<=n-3; i++) {
+        for(int j=0; j<=n-3; j++) {
+            int num1 = 0;
+            for(int k=0; k<3; k++) {
+                for(int l=0; l<4; l++) {
+                    if(arr[i+k][j+l] == 1) {
+                        num1++;
+                    }
+                }
+            }
+            if(ans < num1) {
+                ans = num1;
+            }
+        }
+    }
+
+    cout<<ans<<"\n";
+
+    return 0;
+}
