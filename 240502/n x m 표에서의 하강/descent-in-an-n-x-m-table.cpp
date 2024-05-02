0eac01e578aa6ba43523ed8250b4104ff4801732
HanDaYeon/한다연 <75533232+HanDaYeon-coder@users.noreply.github.com>
2024-05-02 15:49:39 +0900

    Title: n x m 표에서의 하강, Time: 1087ms, Memory: 1MB, Status: Time Limit Exceed - CodeTree

---
+++ 240502/n x m 표에서의 하강/descent-in-an-n-x-m-table.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+int ans = 1e9;
+int arr[504][504];
+int visited[504][504]; 
+int dx[3] = {1,1,1};
+int dy[3] = {0,1,-1};
+
+void init_visited(){
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++) {
+            visited[i][j]=0;
+        }
+    }
+}
+
+void dfs(int x, int y, int dir, int cost){ //dir : 남쪽 = 1, 남서쪽 = 2, 남동쪽 = 3
+    visited[x][y] = 1;
+
+    if(x==n-1){
+        ans = min(ans, cost);
+    }
+
+    for(int i=0; i<3; i++){
+        if(dir == i) continue;
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+
+        if(nx < 0 or ny <0 or nx >= n or ny >= m) continue;
+        if(visited[nx][ny]==0){
+            visited[nx][ny] = 1;
+            dfs(nx,ny,i, cost+arr[nx][ny]);
+            visited[nx][ny] = 0;
+        }
+    }
+
+    return;
+}
+
+int main() {
+    cin >> n >> m;
+
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    for(int i=0; i<m; i++) {
+        dfs(0,i,-1, arr[0][i]);
+    }
+
+    cout<<ans<<"\n";
+
+    return 0;
+}
