dfc82cab3f098e0272325a8708ad5bbe839f61ac
HanDaYeon/한다연 <75533232+HanDaYeon-coder@users.noreply.github.com>
2024-05-02 15:52:43 +0900

    Title: n x m 표에서의 하강, Time: 1004ms, Memory: 1MB, Status: Time Limit Exceed - CodeTree

--- 240502/n x m 표에서의 하강/descent-in-an-n-x-m-table.cpp
+++ 240502/n x m 표에서의 하강/descent-in-an-n-x-m-table.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <queue>
 #include <algorithm>
 using namespace std;
 
@@ -21,7 +20,7 @@ void init_visited(){
 void dfs(int x, int y, int dir, int cost){ //dir : 남쪽 = 1, 남서쪽 = 2, 남동쪽 = 3
     visited[x][y] = 1;
 
-    if(cost > ans){
+    if(cost >= ans){
         return;
     }
     if(x==n-1){
