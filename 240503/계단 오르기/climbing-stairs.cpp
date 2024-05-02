16b3f3b083060e7c944aa55918de953c0214d0cc
HanDaYeon/한다연 <75533232+HanDaYeon-coder@users.noreply.github.com>
2024-05-03 01:10:55 +0900

    Title: 계단 오르기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240503/계단 오르기/climbing-stairs.cpp
+++ 240503/계단 오르기/climbing-stairs.cpp
@@ -5,10 +5,21 @@ using namespace std;
 int n;
 int arr[1004];
 
-void func1(){
+void func(){
+    arr[0] = 0;
+    arr[1] = 0;
+    arr[2] = 1;
+    arr[3] = 1;
+
+    for(int i=4; i<=n; i++) {
+        arr[i] = (arr[i-2] + arr[i-3]) % 10007;
+    }
+}
+
+void func1() {
     queue<int> que;
     que.push(0);
-    arr[0] = 1;
+    arr[0]=1;
 
     while(!que.empty()){
         int x = que.front();
@@ -22,16 +33,15 @@ void func1(){
             arr[x+2] = arr[x];
             que.push(x+2);
         } else {
-            arr[x+2] += arr[x];
+            arr[x+2] = (arr[x] + arr[x+2]) % 10007;
         }
 
         if(arr[x+3]==0){
             arr[x+3] = arr[x];
             que.push(x+3);
         } else {
-            arr[x+3] += arr[x];
+            arr[x+3] = (arr[x] + arr[x+3]) % 10007;
         }
-
     }
 }
 
@@ -39,6 +49,7 @@ int main() {
     cin >> n;
 
     func1();
+
     cout<<arr[n]<<"\n";
 
     return 0;
