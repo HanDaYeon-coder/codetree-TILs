c34cfc350e8cdb4f72da7983692282f3fc1c1c3a
HanDaYeon/한다연 <75533232+HanDaYeon-coder@users.noreply.github.com>
2024-05-03 17:22:48 +0900

    Title: 블럭쌓는 명령, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240503/블럭쌓는 명령/block-stacking-commands.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, k;
+int a, b;
+int arr[1000004];
+
+int main() {
+    cin >> n >> k;
+
+    for(int i=0; i<k; i++) {
+        cin >> a >> b;
+        for(int j=a; j<b; j++){
+            arr[j] += 1;
+        }
+    }
+
+    sort(arr, arr+n);
+
+    cout<<arr[(n+2)/2]<<"\n";
+
+    return 0;
+}
