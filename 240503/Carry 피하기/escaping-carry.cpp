d8ada5bcdb1cb8b3069ae1da036160221a40e2d5
HanDaYeon/한다연 <75533232+HanDaYeon-coder@users.noreply.github.com>
2024-05-03 17:07:11 +0900

    Title: Carry 피하기, Time: 243ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240503/Carry 피하기/escaping-carry.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n;
+vector<int> v;
+string arr[24];
+int ans = 0;
+
+void combi(int start, int r){
+    if(v.size()==r){
+        int cnt = 0;
+
+        for(int j=0; j<9; j++){
+            int sum = 0;
+            
+            for(auto i : v){
+                sum += (arr[i][j]-'0');
+            }
+            if(sum<10){
+                cnt++;
+            }
+        }
+
+        if(cnt==9){
+            ans = r;
+        }
+        return;
+    }
+
+    for(int i=start+1; i<n; i++){
+        v.push_back(i);
+        combi(i, r);
+        v.pop_back();
+    }
+}
+
+int main() {
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    for(int i=0; i<n; i++){
+        int length = 9-arr[i].size();
+        string zero_str = "";
+
+        for(int j=0; j<length; j++){
+            zero_str += '0';
+        }
+        
+        arr[i] = zero_str + arr[i];
+    }
+
+    for(int i=n; i>=1; i--){
+        combi(-1,i);
+
+        if(ans!=0){
+            cout<<i<<"\n";
+            break;
+        }
+    }
+    return 0;
+}
